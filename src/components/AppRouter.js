1b3fb34266ee36423b4c3eed3bbd05fb411b6bbc
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2019-09-14 09:05:38 -0400

    added approuter and nav

---
+++ src/components/AppRouter.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route } from 'react-router-dom';
+import CharacterList from './CharacterList';
+import LocationsList from './LocationsList';
+import EpisodesList from './EpisodeList';
+import WelcomePage from './WelcomePage';
+
+
+
+export default function AppRouter() {
+    return (
+        <div>
+            <Route exact path="/" component={WelcomePage} />
+            <Route path="/characters" component={CharacterList} />
+            <Route path="/location" component={LocationsList} />
+            <Route path="/episode" component={EpisodesList} />
+
+        </div>
+    )
+}
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
+import styled from 'styled-components';
 
 // TODO: Add missing menu/tabs/nav below
 
@@ -10,6 +11,25 @@ import { NavLink } from "react-router-dom";
 // https://react.semantic-ui.com/elements/button/
 // https://react.semantic-ui.com/collections/breadcrumb/
 
+
 export default function TabNav() {
+    return (
+        <div>
+            <nav>
+                <NavLink exact to="/" >
+                    Home
+                </NavLink>
+                <NavLink exact to="/characters" >
+                   Characters
+                </NavLink>
+                <NavLink exact to="/locations" >
+                    Locations
+                </NavLink>
+                <NavLink exact to="/episodes" >
+                   Episodes
+                </NavLink>
+            </nav>
+        </div>
+    )
 
 };
